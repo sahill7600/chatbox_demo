c9b0bde56c17a8889c3a170720f9fe2576131817
sahill7600 <joee.pennn@gmail.com>
2024-06-10 15:50:53 +0530

    YOUR FIRST MESSAGE HERE

---
+++ android/app/src/main/kotlin/com/example/chatbox_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatbox_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Authenticate/Authenticate/Autheticate.dart
@@ -0,0 +1,18 @@
+import 'package:chatbox_demo/Authenticate/Authenticate/LoginScree.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../Screens/HomeScreen.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
